d1d5f041795bf9e86fe5ff6a521e3a043449d14a
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-15 12:00:35 -0300

    web: make karma use edge by default

--- src/Web/src/app/components/authentication-page/authentication-page.component.spec.ts
+++ src/Web/src/app/components/authentication-page/authentication-page.component.spec.ts
@@ -1,22 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AuthenticationPageComponent } from './authentication-page.component';
+import { AuthenticationPageComponent } from "./authentication-page.component";
+import { appConfig } from "../../app.config";
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from "@angular/common/http/testing";
 
-describe('AuthenticationPageComponent', () => {
+describe("AuthenticationPageComponent", () => {
   let component: AuthenticationPageComponent;
   let fixture: ComponentFixture<AuthenticationPageComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AuthenticationPageComponent],
+      providers: [provideHttpClientTesting(), ...appConfig.providers],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthenticationPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/Web/src/app/services/user/user.service.spec.ts
+++ src/Web/src/app/services/user/user.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { UserService } from './user.service';
+import { UserService } from "./user.service";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
-describe('UserService', () => {
+describe("UserService", () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(withFetch())],
+    });
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
