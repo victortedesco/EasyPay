988e62916183ec278929f30ec674748c58a063d3
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-15 12:00:35 -0300

    web: make karma use edge by default

--- src/Web/src/app/components/home-page/home-page.component.spec.ts
+++ src/Web/src/app/components/home-page/home-page.component.spec.ts
@@ -1,22 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HomePageComponent } from './home-page.component';
+import { HomePageComponent } from "./home-page.component";
+import { appConfig } from "../../app.config";
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from "@angular/common/http/testing";
+import { UserService } from "../../services/user/user.service";
 
-describe('HomePageComponent', () => {
+describe("HomePageComponent", () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let userService: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomePageComponent],
+      providers: [provideHttpClientTesting(), ...appConfig.providers],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    userService = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
