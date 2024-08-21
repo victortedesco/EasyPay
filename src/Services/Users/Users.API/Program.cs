1a4592e2e7952ebd2948805363b13eebc6fb4c41
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-21 14:39:01 -0300

    docker: fix docker compose health check

--- src/Services/Users/Users.API/Program.cs
+++ src/Services/Users/Users.API/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -10,5 +11,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapHealthChecks("/health");
 
 app.Run();
