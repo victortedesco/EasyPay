2a02c172bd304c0f6aaec266f72d36a8fabcea99
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-23 20:48:22 -0300

    user: add service and repository

---
+++ src/Services/Users/Users.API/Services/IUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users.API.Infrastructure.Data;
+using Users.API.Services.Models;
+
+namespace Users.API.Services;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByDocumentAsync(string document);
+}
+
+public class UserRepository(ApplicationDataContext dataContext) : IUserRepository
+{
+    private readonly DbSet<User> _users = dataContext.Users;
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _users.FindAsync(id);
+    }
+
+    public async Task<User?> GetByDocumentAsync(string document)
+    {
+        return await _users.AsNoTracking().FirstOrDefaultAsync(u => u.Document == document);
+    }
+}
