7e3ec66bafc0bad1902330220eee759977af902d
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-23 20:48:22 -0300

    user: add service and repository

---
+++ src/Services/Users/Users.API/Services/IUserService.cs
@@ -0,0 +1,29 @@
+﻿using Users.API.Application.Extensions;
+using Users.API.Services.DTOs;
+
+namespace Users.API.Services;
+
+public interface IUserService
+{
+    Task<UserDTO?> GetByIdAsync(int id);
+    Task<UserDTO?> GetByDocumentAsync(string document);
+}
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<UserDTO?> GetByIdAsync(int id)
+    {
+        var user = await _userRepository.GetByIdAsync(id);
+
+        return user?.ToDTO();
+    }
+
+    public async Task<UserDTO?> GetByDocumentAsync(string document)
+    {
+        var user = await _userRepository.GetByDocumentAsync(document);
+
+        return user?.ToDTO();
+    }
+}
