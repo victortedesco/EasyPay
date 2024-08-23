88ecdfd00c397c770b908bf46478f5f62173f1f2
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-08-23 19:53:22 -0300

    user: add model, dto, view model and extensions

---
+++ src/Services/Users/Users.API/Services/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Users.API.Services.Models;
+
+public class User
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Document { get; private set; }
+    public string Email { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public decimal Balance { get; private set; }
+}
