d0143590debf08011b4cec55d1b23dc69c584585
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-10-23 23:59:28 -0300

    web: keycloak auth fixes

--- src/Web/src/app/app.component.ts
+++ src/Web/src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { APP_INITIALIZER, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConfigService } from './services/config.service';
-import { tap } from 'rxjs';
-import { BrowserModule } from '@angular/platform-browser';
-
-export function initializeApp(configService: ConfigService) {
-  return () =>
-    configService.loadConfig().pipe(
-      tap(config => configService.setConfig(config))
-    ).toPromise();
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BrowserModule, RouterOutlet],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeApp,
-      deps: [ConfigService],
-      multi: true,
-    },
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
